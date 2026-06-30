21573758348b878e2c34fed5cb37f7abc8ed5437
mehtanusha <mehtanusha18@gmail.com>
2026-06-30 23:08:58 +0530

    Time: 470 ms (11.06%), Space: 53.4 MB (13.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-
-        int maxReach = 0;
-
-        for(int i=0;i<nums.size();i++) {
-
-            if(i > maxReach)
-                return false;
-
-            maxReach = max(maxReach,
-                           i + nums[i]);
+    int dp[10001];
+    bool solve(vector<int>&nums,int n ,int idx){
+        if(idx == n-1){
+            return true;
         }
-
-        return true;
+        if(idx >= n) return false;
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        
+        for(int i=1;i<=nums[idx];i++){
+            if(solve(nums,n,idx+i) == true) return true;
+        }
+        return dp[idx] = false;
+    }
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,n,0);
     }
 };
