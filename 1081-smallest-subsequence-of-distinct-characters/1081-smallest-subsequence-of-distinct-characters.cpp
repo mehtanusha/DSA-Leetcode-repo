0e4593b4e217603786bbcc900d4f9a82ba229180
Anusha Mehta <mehtanusha18@gmail.com>
2026-07-19 19:06:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (57.91%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+
+        vector<int> freq(256, 0);
+        vector<bool> visited(256, false);
+        stack<char> st;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        for (char ch : s) {
+
+            // Current character ko process kar rahe hain
+            freq[ch]--;
+
+            // Agar already stack me hai to skip
+            if (visited[ch]) continue;
+
+            // Jab tak:
+            // 1. Stack empty nahi hai
+            // 2. Stack ka top current se bada hai
+            // 3. Top character future me dobara milega
+            while (!st.empty() &&
+                   st.top() > ch &&
+                   freq[st.top()] > 0) {
+
+                visited[st.top()] = false;
+                st.pop();
+            }
+
+            st.push(ch);
+            visited[ch] = true;
+        }
+
+        string ans = "";
+
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
