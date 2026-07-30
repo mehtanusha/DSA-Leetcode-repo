27223dc18894ab383089e2ed44c3a94d7fd7ba81
Anusha Mehta <mehtanusha18@gmail.com>
2026-07-30 23:25:55 +0530

    Time: 6 ms (52.07%), Space: 104.7 MB (68.13%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -1,49 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Info {
-    public : 
-    int leftmax,rightmin,sum;
+public:
+    int minNode, maxNode, sum;
 
-    Info(int leftmax,int rightmin,int sum){
-        this->leftmax = leftmax;
-        this->rightmin = rightmin;
+    Info(int minNode, int maxNode, int sum) {
+        this->minNode = minNode;
+        this->maxNode = maxNode;
         this->sum = sum;
     }
 };
+
 class Solution {
 public:
     int ans = 0;
 
-    Info helper(TreeNode* root){
-        if(root == nullptr) return  Info(INT_MIN,INT_MAX,0);
+    Info helper(TreeNode* root) {
+
+        if (root == nullptr)
+            return Info(INT_MAX, INT_MIN, 0);
 
         auto left = helper(root->left);
         auto right = helper(root->right);
 
-        if(left.leftmax < root->val && root->val < right.rightmin){
-            int currsum = left.sum + right.sum + root->val;
+        if (left.maxNode < root->val &&
+            root->val < right.minNode) {
 
-            ans = max(ans,currsum);
+            int currSum = left.sum + right.sum + root->val;
 
-            return Info(max(root->val,right.leftmax),min(root->val,left.rightmin),currsum);
+            ans = max(ans, currSum);
+
+            return Info(
+                min(root->val, left.minNode),
+                max(root->val, right.maxNode),
+                currSum
+            );
         }
-        else{
-            return Info(INT_MAX,INT_MIN,0);
-        }
+
+        return Info(INT_MIN, INT_MAX, 0);
     }
 
     int maxSumBST(TreeNode* root) {
-       helper(root);
-       return ans; 
+        helper(root);
+        return ans;
     }
 };
