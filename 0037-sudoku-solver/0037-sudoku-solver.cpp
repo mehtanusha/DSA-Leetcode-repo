8f891cb4fcd8886ec145211325889694ca917fa6
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-08 07:59:31 +0530

    Time: 363 ms (51.02%), Space: 9 MB (11.65%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+    bool isvalid(int row, int col, char digit,
+                 vector<vector<char>>& board) {
+
+        for(int i = 0; i < 9; i++) {
+
+            if(board[i][col] == digit)
+                return false;
+
+            if(board[row][i] == digit)
+                return false;
+        }
+
+        int start_i = (row / 3) * 3;
+        int start_j = (col / 3) * 3;
+
+        for(int k = 0; k < 3; k++) {
+            for(int m = 0; m < 3; m++) {
+
+                if(board[start_i + k][start_j + m] == digit)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+
+                if(board[i][j] == '.') {
+
+                    for(char d = '1'; d <= '9'; d++) {
+
+                        if(isvalid(i, j, d, board)) {
+
+                            board[i][j] = d;
+
+                            if(solve(board))
+                                return true;
+
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
