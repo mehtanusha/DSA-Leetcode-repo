ff794edc94f7907c042fcb790b52c07375969d3b
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-14 10:56:15 +0530

    Time: 0 ms (100%), Space: 14 MB (58.76%) - LeetHub

---
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int,int>freq;
+
+        int n = arr.size();
+        int maxi = -1;
+        for(int i=0;i<n;i++){
+            freq[arr[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(freq[arr[i]] == arr[i]){
+                maxi = max(maxi,freq[arr[i]]);
+            }
+        }
+        return maxi;
+    }
+};
