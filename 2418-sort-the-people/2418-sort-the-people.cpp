7d3246c5f6ff5d10e0e28f558e0021d2096d3f3f
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-15 08:02:25 +0530

    Time: 12 ms (51%), Space: 31.4 MB (17.05%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string>mp;
+        int n = names.size();
+        for(int i=0;i<n;i++){
+            mp[heights[i]] = names[i];
+        }
+        sort(heights.rbegin(),heights.rend());
+        vector<string>ans;
+
+        for(int h : heights){
+            ans.push_back(mp[h]);
+        }
+
+        return ans;
+    }
+};
