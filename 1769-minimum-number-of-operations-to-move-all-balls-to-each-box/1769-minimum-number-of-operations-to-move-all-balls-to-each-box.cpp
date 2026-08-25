92579d333c8dd72402232b658228037175dfb78f
Anusha Mehta <mehtanusha18@gmail.com>
2026-08-25 10:51:28 +0530

    Time: 3 ms (68.97%), Space: 12.1 MB (84.57%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -4,15 +4,28 @@ public:
         //brute force
         int n = boxes.size();
         vector<int> ans(n, 0);
-
+        
+        //left to right
+        int balls = 0;
+        int operations = 0;
         for(int i = 0; i < n; i++) {
-            
-            for(int j = 0; j < n; j++) {
-                
-                if(boxes[j] == '1') {
-                    ans[i] += abs(i - j);
-                }
+            ans[i] += operations;
+
+            if(boxes[i] == '1'){
+                balls++;
             }
+            operations += balls;
+        }
+
+         balls = 0;
+         operations = 0;
+        for(int i = n-1; i>=0; i--) {
+            ans[i] += operations;
+
+            if(boxes[i] == '1'){
+                balls++;
+            }
+            operations += balls;
         }
 
         return ans;
