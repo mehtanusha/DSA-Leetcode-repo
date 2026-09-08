b72e5e35f5dd961c5985070e6b6d9b40e8b8d3b4
Anusha Mehta <mehtanusha18@gmail.com>
2026-09-08 17:56:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int cnt = 0;
-        while( n>999 && n<=100000){
-            cnt++;
-            n--;
-        }
-        return cnt;
+       return max(n-999,0);
     }
 };
