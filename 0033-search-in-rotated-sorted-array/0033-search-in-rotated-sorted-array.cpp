b9c90b31db1da9ef16fd27dd0caeae76326089f0
Anusha Mehta <mehtanusha18@gmail.com>
2026-09-12 00:28:43 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,47 +1,29 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums,int start,int end,int target){
+    int search(vector<int>& nums, int target) {
+        int start =0;
+        int end = nums.size() - 1;
         while(start<=end){
-            int mid = start + (end -start)/2;
-            if(nums[mid] == target){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid] < target){
-                start = mid + 1;
-            }else{
-                end = mid -1;
             }
-        }
-        return -1;
-    }
-    int rotated(vector<int>& nums){
-        int n = nums.size();
-        int start = 0;
-        int end = n-1;
-        while(start < end){
-            int mid = start + (end - start)/2;
-
-            if(nums[mid] > nums[end]){
-                start = mid + 1;
+            else if(nums[start]<=nums[mid]){
+                if(target >= nums[start] && target < nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid +1;
+                }
             }
             else{
-                end = mid;
+                if(target > nums[mid] && target <= nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
             }
-        } return start;
-    }
-    int search(vector<int>& nums, int target) {
-         
-        int n = nums.size();
-
-        int index = rotated(nums);
-        int ans = -1;
-
-        if (index == 0) { 
-            return binarySearch(nums, 0, n - 1, target);
-        }
-
-        if(target >= nums[0] && target <= nums[index-1]){
-            return binarySearch(nums,0,index-1,target);
-        }
-        return binarySearch(nums,index,n-1,target);
+        } return -1;
     }
 };
